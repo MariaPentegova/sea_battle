6c531f8d60a39401450e6531ad2f69f9159e34b7
MariaPentegova <m.pentegova@yandex.ru>
2026-06-04 23:28:27 +0300

    Update move.kt

--- src/main/kotlin/models/move.kt
+++ src/main/kotlin/models/move.kt
@@ -1,5 +1,15 @@
 package models
 
+data class GameMove(
+    val playerId: Int,
+    val row: Int,
+    val col: Int,
+    val isHit: Boolean,
+    val isKill: Boolean,
+    val moveNumber: Int,
+    val timestamp: Long
+)
+
 enum class MoveResult {
     HIT,
     MISS,
